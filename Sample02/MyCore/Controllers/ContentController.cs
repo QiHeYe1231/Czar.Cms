3a3c54c3106d3b0430d6c5a3b8b9423465e8cdaf
zhouying <413969949@qq.com>
2019-09-27 18:19:15 +0800

    新增项目

---
+++ Sample02/MyCore/Controllers/ContentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MyCore.Models;
+using static MyCore.Models.ContentBase;
+
+namespace MyCore.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
